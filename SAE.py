ced6b38661badef03bc04d25f9224bcc87cea090
Shay Dineen <sdineen1@villanova.edu>
2018-06-12 18:17:19 -0400

    Fixing another error

--- SAE.py
+++ SAE.py
@@ -140,15 +140,15 @@ def build_SAE(layers, data, activation, regularizer, batch_size, epochs, optim):
     #Creating a model that pools all of learned weights together 
     #Wasn't sure if I should add the outputs of the fourth sae to the model
     sae_input = Input(shape=(layers[0], )) #
-    sae_en1 = Dense(units = layers[1], activation=activation, activity_regularizer=regularizers.l1(.05))(sae_input)'''
-    sae_en2 = Dense(units = layers[2], activation=activation, activity_regularizer=regularizers.l1(.025))(sae_en1)
+    sae_en1 = Dense(units = layers[1], activation=activation, activity_regularizer=regularizers.l1(.05))(sae_input)
+    '''sae_en2 = Dense(units = layers[2], activation=activation, activity_regularizer=regularizers.l1(.025))(sae_en1)
     sae_en3 = Dense(units = layers[3], activation=activation, activity_regularizer=regularizers.l1(.0125))(sae_en2)
     sae_en4 = Dense(units = layers[4], activation=activation, activity_regularizer=regularizers.l1(.0075))(sae_en3)'''
     #the same reguklarizer sparsity parameter nneds to be chanfe for easch lasyer 
 
     sae = Model(sae_input, sae_en1)
-    sae.layers[1].set_weights(ae1_weights)'''
-    sae.layers[2].set_weights(ae2_weights)
+    sae.layers[1].set_weights(ae1_weights)
+    '''sae.layers[2].set_weights(ae2_weights)
     sae.layers[3].set_weights(ae3_weights)
     sae.layers[4].set_weights(ae4_weights)
     
