d9ba4a480b203bdde0b01b417a5b12288dc16503
Shay Dineen <sdineen1@villanova.edu>
2018-06-27 09:04:26 -0400

    fixing an error

--- LSTM.py
+++ LSTM.py
@@ -16,7 +16,7 @@ data = pd.read_csv('SAEoutput.csv', engine= 'python', encoding = 'ASCII' )
 data = np.array(data)
 
 #In the paper, Bao, Yue, and Rao s training set consisted of 80% of the data while the CV and test sets each consisted of 10% of the data
-training_size = int(math.floor(len(data)*.6)) #going to use .6 because i trained the SAE on 60% of the data
+training_size = int(math.floor(len(data)*.8)) #going to use .6 because i trained the SAE on 60% of the data
 #cv_size = math.ceil(len(data)*.1)
 #test_size = math.ceil(len(data)*.1)
 
@@ -44,8 +44,8 @@ time_steps = 90 #arbitraily set the # of timesteps to 60.  The paper does not sp
 #based on  the way they trained their model the # number of time_steps that they used is between 0 and 720 (not very helpful)
 
 for i in range (time_steps, len(training_set_scaled)):
-    X_train.append(training_set_scaled[i-time_steps:i, 8:16])   #training_set_scaled
-    y_train.append(training_set_scaled[i, 8])                   #training_set_scaled
+    X_train.append(training_set_scaled[i-time_steps:i, 0:8])   #training_set_scaled
+    y_train.append(training_set_scaled[i, 0])                   #training_set_scaled
 
 X_train , y_train = np.array(X_train), np.array(y_train) #Transforiming the list objects into numpy arrays 
 
@@ -115,10 +115,10 @@ inputs = data[len(data)-test_size-time_steps:]
 inputs = sc.transform(inputs)
 X_test = []
 for i in range (time_steps , len(inputs)):
-    X_test.append(inputs[i-time_steps:i, 8:16])
+    X_test.append(inputs[i-time_steps:i, 0:8])
     
 X_test = np.array(X_test)
-y_test = inputs[time_steps:len(inputs),8]
+y_test = inputs[time_steps:len(inputs),0]
 
 y_pred = regressor.predict(X_test)
 
