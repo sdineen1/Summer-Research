c1be480dd0559f3f0548f2e4cff72d4f0f766292
Shay Dineen <sdineen1@villanova.edu>
2018-07-09 13:01:46 -0400

    error

--- SAE.py
+++ SAE.py
@@ -278,9 +278,9 @@ def two_hidden_layer_AE(data, X_train):
 
 output, hidden_output_func = two_hidden_layer_AE(data = data, X_train = X_train)
 
-compressed_data = hidden_output_func([data])[0]
+#compressed_data = hidden_output_func([data])[0]
 filepath = 'SAEoutput.csv'
-df = pd.DataFrame(compressed_data)
+df = pd.DataFrame(output)
 df.to_csv(filepath, index=False)
             
             
