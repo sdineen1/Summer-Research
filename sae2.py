92ff15387f8b36b99c934c2497967dd1915809a1
Shay Dineen <sdineen1@villanova.edu>
2018-08-03 10:40:42 -0400

    run

--- sae2.py
+++ sae2.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import math as math
 
-dataset = pd.read_csv('ETF_Opportunity_Set/SameTrain/tip.csv')
+dataset = pd.read_csv('ETF_Opportunity_Set/SameTrain/tlt.csv')
 data = dataset.iloc[:,1:].values
 data = np.array(data)
 
